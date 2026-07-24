838c38792de8eec4a8515de34af4e39f86ffafb9
jeffreykeddaue5 <jeffreykeddaue5@gmail.com>
2026-07-23 20:40:21 -0400

    Vulkan Triangle: Implements a vulkan instance

--- notes/setup.md
+++ notes/setup.md
@@ -1,3 +1,37 @@
 # Setup 
 
+# Vulkan Instance 
+-- A vulkan instance is the pathway between the program and the vulkan api
+-- First we need to specifiy some details to the vulkan gpu driver 
+
+-- Vulkan Program -> Vulkan SDK Headers/ Loader -> Vulkan.so -> Vulkan GPU driver 
+
+-- SDK (Software development tool kit)
+-- contains: headers files (vulkan/vulkan.h)/ libraries (Vulkan loader)/ 
+Validation Layers / Debugging tools / Shader tools / Documentation Examples
+
+
+-- Vulkan loader (libvulkan.so)
+a piece of sofware that lives in between your vulkan application and vulkan driver 
+-- it is responsible for communicating details about your application to the vulkan driver
+
+
+-- Vulkan driver -- interprets vulkan commands so that the gpu understands
+(Vulkan Capable Graphics driver)
+
+-- For AMD, Drivers are Mesa RADV or AMDVLK 
+
+
+# platform agnostic : not tied to a specific operating system/hardware/environment; code looks the same on all computers
+
+therefore, vulkan is platform agnostic
+
+-- however, creating a window generally is platform specific because the code to create a window depends on the operating system
+
+-- glfw helps us out with that by dealing with this under the hood and the platform specific details
+
+-- extension is an optinal feature that extends Vulkan's core api/ functionality
+    -- think of it as a plugin that can be platform specific (linking the glfw window library with VK_KHR_surface)
+    -- necessary so we don't include libraries that are uneeded with the core api that might be platform specific (thus being non platform agnostic)
+
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,10 @@
+#include "vulkan/vulkan.hpp"
+#include <algorithm>
 #include <cstdint>
+#include <cstring>
+#include <ostream>
+#include <stdexcept>
+#include <vulkan/vulkan_core.h>
 #if defined(__INTELLISENSE__) || !defined(USE_CPP20_MODULES)
 #include <vulkan/vulkan_raii.hpp>
 #else
@@ -24,8 +30,57 @@ class HelloTriangleApplication {
 
     private:
         GLFWwindow *window = nullptr;
+        vk::raii::Context context;
+        vk::raii::Instance instance = nullptr;
 
-        void initVulkan() {}
+        void createInstance() {
+            vk::ApplicationInfo appinfo;
+            appinfo.pApplicationName = "Vulkan Triangle";
+            appinfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+            appinfo.pEngineName = "No Engine";
+            appinfo.apiVersion = vk::ApiVersion14;
+
+            uint32_t glfwExtensionCount = 0;
+            auto glfwExtensions =
+                glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+            auto extensionProperties =
+                context.enumerateInstanceExtensionProperties();
+
+            // Available extensions
+            std::cout << "available extensions:" << std::endl;
+            for (const auto &extension : extensionProperties) {
+                std::cout << '\t' << extension.extensionName << '\n';
+            }
+
+            // Checks whether vulkan supports the necessary extensions for glfw
+            for (uint32_t i = 0; i < glfwExtensionCount; ++i) {
+                if (!std::ranges::any_of(
+                        extensionProperties,
+                        [glfwExtension =
+                             glfwExtensions[i]](auto const &extensionProperty) {
+                            return strcmp(extensionProperty.extensionName,
+                                          glfwExtension) == 0;
+                        })) {
+                    throw std::runtime_error(
+                        "Required GLFW extension not supported: " +
+                        std::string(glfwExtensions[i]));
+                } else {
+                    std::cout
+                        << "Extension found: " << std::string(glfwExtensions[i])
+                        << std::endl;
+                }
+            }
+
+            vk::InstanceCreateInfo createinfo;
+            createinfo.pApplicationInfo = &appinfo;
+            createinfo.enabledExtensionCount = glfwExtensionCount;
+            createinfo.ppEnabledExtensionNames = glfwExtensions;
+
+            instance = vk::raii::Instance(context, createinfo);
+        }
+
+        void initVulkan() { createInstance(); }
 
         void initWindow() {
             glfwInit();
